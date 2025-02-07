6a49d51e8ac1cb3c6e76bddd4b3da650154daf69
mihalacheD <danutza_dima88@yahoo.com>
2025-02-07 17:34:58 +0200

    Improve user experience MovieGrid

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -18,11 +18,19 @@ const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 if (error) return <Text>{error}</Text>;
 
+
   return (
+    <>
+    {!isLoading && movies.length === 0 && (
+      <Text fontSize="xl" fontWeight="bold" textAlign="center" mt={4}>
+        No movies found
+      </Text>
+    )}
     <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4}}  gap={6}>
       {isLoading && skeletons.map(skeleton => <MovieCardSkeleton key={skeleton}/>)}
       {movies.map( movie => <MovieCard key={movie.id} movie={movie}/>)}
     </SimpleGrid>
+    </>
   )
 }
 
