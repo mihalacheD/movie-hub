c2e17a41af2898a9752014b7eff8561366882f52
mihalacheD <danutza_dima88@yahoo.com>
2025-02-07 17:24:07 +0200

    Refactoring Game Grid

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,5 +1,5 @@
 import useMovies from "@/hooks/useMovies";
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import MovieCard from "./MovieCard";
 import { Genre } from "@/hooks/useGenres";
 import MovieCardSkeleton from "./MovieCardSkeleton";
@@ -16,14 +16,13 @@ const MovieGrid = ({ selectedGenre, sortOption, searchText} : Props) => {
 const { movies, error, isLoading } = useMovies(selectedGenre, sortOption, searchText);
 const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-    {error && <p>{error}</p>}
     <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4}}  gap={6}>
       {isLoading && skeletons.map(skeleton => <MovieCardSkeleton key={skeleton}/>)}
       {movies.map( movie => <MovieCard key={movie.id} movie={movie}/>)}
     </SimpleGrid>
-    </>
   )
 }
 
